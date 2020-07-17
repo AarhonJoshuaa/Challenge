9998de18860fc8e65f9a431e9e83fa390f15b90c
Aarhon <aarhonjoshuaa001@gmail.com>
2020-07-17 13:13:52 +0530

    Loading

--- challenge_completed.py
+++ challenge_completed.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from tamil import utf8
 from itertools import count 
-
+import winsound
 URL = 'https://venmurasu.in/2014/01/01/%e0%ae%b5%e0%af%86%e0%ae%a3%e0%af%8d%e0%ae%ae%e0%af%81%e0%ae%b0%e0%ae%9a%e0%af%81-%e0%ae%a8%e0%af%82%e0%ae%b2%e0%af%8d-%e0%ae%92%e0%ae%a9%e0%af%8d%e0%ae%b1%e0%af%81/'
 final=[]
 new=[]
 while True:
-    f = open("a.txt", "w",encoding="utf-8")
+    f = open("output.txt", "w",encoding="utf-8")
     page = requests.get(URL)
     
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -28,7 +28,7 @@ while True:
         a=a.replace('“','')
         a=a.replace('!','',10000000000)
         a=a.replace('.','',10000000000)
-        a=a.replace(',','',10000000000)
+        a=a.replace(',','',10000000000) 
         a=a.replace('?','',10000000000)
         a=a.replace('ஏற்றப்படுகின்றது','',10000000000)
         if(utf8.all_tamil(a) ):
@@ -50,3 +50,4 @@ while True:
     f.write(href['href'])
     f.write('\n')
 f.close()
+winsound.Beep(2000, 1000)
